197dd16baadefb4bb5a749900d2a1630abc1cd25
gack951 <gack951@gmail.com>
2012-11-28 06:04:27 +0900

    プロジェクト作成

---
+++ README.md
@@ -0,0 +1,2 @@
+Android用素因数分解アプリです。
+Created by gack951
---
+++ src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
@@ -0,0 +1,13 @@
+package com.gack951.Prime_Factorization;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Prime_FactorizationActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
