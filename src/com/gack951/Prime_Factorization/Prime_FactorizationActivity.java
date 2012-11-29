13afca700cef997f0bbea075d01f67232ea305a4
gack951 <gack951@gmail.com>
2012-11-29 11:49:21 +0900

    formated code

--- src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
+++ src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
@@ -6,14 +6,12 @@ import android.widget.*;
 import android.view.*;
 import android.view.View.*;
 
-public class Prime_FactorizationActivity extends Activity implements OnClickListener
-{
+public class Prime_FactorizationActivity extends Activity implements OnClickListener{
 
     /** Called when the activity is first created. */
     @Override
 
-    public void onCreate(Bundle savedInstanceState)
-	{
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
@@ -25,79 +23,63 @@ public class Prime_FactorizationActivity extends Activity implements OnClickList
 		clear_button.setOnClickListener(this);
     }
 
-	public void onClick(View v)
-	{
+	public void onClick(View v){
 		TextView textview1=(TextView)findViewById(R.id.textview1);
 		EditText edit1=(EditText)findViewById(R.id.edit1);
-		switch (v.getId())
-		{
+		switch (v.getId()){
 			case R.id.show_button:
 				long num,w=3,sqrt_num;
 				int i=0,j=1;
 				String output_temp="";
 				long[][] factors = new long[20][2];
 
-				try
-				{
+				try{
 					num = Long.parseLong(edit1.getText().toString());
 				}
-				catch (NumberFormatException e)
-				{
+				catch (NumberFormatException e){
 					return;
 				}
 				output_temp = Long.toString(num) + "=";
-				sqrt_num=(long)(Math.sqrt(num));
+				sqrt_num = (long)(Math.sqrt(num));
 				//textview1.setText(output_temp + " " + Long.toString(sqrt_num));
 
-				if (num % 2 == 0)
-				{
-					while (num % 2 == 0)
-					{
+				if (num % 2 == 0){
+					while (num % 2 == 0){
 						factors[i][0] = 2;
 						factors[i][1]++;
 						num /= 2;
 					}
-					sqrt_num=(long)(Math.sqrt(num));
+					sqrt_num = (long)(Math.sqrt(num));
 					i++;
 				}
-				while (w <= sqrt_num)
-				{
-					if (num % w == 0)
-					{
+				while (w <= sqrt_num){
+					if (num % w == 0){
 						factors[i][0] = w;
-						while (num % w == 0)
-						{
+						while (num % w == 0){
 							factors[i][1]++;
 							num /= w;
 						}
-						sqrt_num=(long)(Math.sqrt(num));
+						sqrt_num = (long)(Math.sqrt(num));
 						i++;
 					}
 					w += 2;
 				}
 
-				if (i == 0)
-				{
+				if (i == 0){
 					output_temp += "(素数)";
-				}
-				else
-				{
+				}else{
 					output_temp += Long.toString(factors[0][0]);
-					if (factors[0][1] > 1)
-					{
+					if (factors[0][1] > 1){
 						output_temp += "^" + Long.toString(factors[0][1]);
 					}
-					while (j < i)
-					{
+					while (j < i){
 						output_temp += "×" + Long.toString(factors[j][0]);
-						if (factors[j][1] > 1)
-						{
+						if (factors[j][1] > 1){
 							output_temp += "^" + Long.toString(factors[j][1]);
 						}
 						j++;
 					}
-					if (num != 1)
-					{
+					if (num != 1){
 						output_temp += "×" + Long.toString(num);
 					}
 				}
