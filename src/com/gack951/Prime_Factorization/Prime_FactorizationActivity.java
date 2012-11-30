e4d946a8cf728bfcac905e59da23ffbf0b776114
gack951 <gack951@gmail.com>
2012-12-01 07:04:56 +0900

    Fixed letters of messages

--- src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
+++ src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
@@ -76,14 +76,14 @@ public class Prime_FactorizationActivity extends Activity implements OnClickList
 					output_temp += " ^ " + Long.toString(factors[0][1]);
 				}
 				while (j < i) {
-					output_temp += " ÁE" + Long.toString(factors[j][0]);
+					output_temp += " × " + Long.toString(factors[j][0]);
 					if (factors[j][1] > 1) {
 						output_temp += " ^ " + Long.toString(factors[j][1]);
 					}
 					j++;
 				}
 				if (num != 1) {
-					output_temp += " ÁE" + Long.toString(num);
+					output_temp += " × " + Long.toString(num);
 				}
 			}
 			textview1.setText(output_temp + "\n\n" + textview1.getText().toString());
