775180ea82e52ea45530301bbd3ed01af3c0bd35
gack951 <gack951@gmail.com>
2012-11-30 23:34:11 +0900

    Fixed import

--- src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
+++ src/com/gack951/Prime_Factorization/Prime_FactorizationActivity.java
@@ -4,9 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 
 public class Prime_FactorizationActivity extends Activity implements OnClickListener {
 
@@ -78,14 +76,14 @@ public class Prime_FactorizationActivity extends Activity implements OnClickList
 					output_temp += " ^ " + Long.toString(factors[0][1]);
 				}
 				while (j < i) {
-					output_temp += " × " + Long.toString(factors[j][0]);
+					output_temp += " ÁE" + Long.toString(factors[j][0]);
 					if (factors[j][1] > 1) {
 						output_temp += " ^ " + Long.toString(factors[j][1]);
 					}
 					j++;
 				}
 				if (num != 1) {
-					output_temp += " × " + Long.toString(num);
+					output_temp += " ÁE" + Long.toString(num);
 				}
 			}
 			textview1.setText(output_temp + "\n\n" + textview1.getText().toString());
